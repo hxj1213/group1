27762045600c2e26b5f6fc1dbbd363fe36f0193a
YQJ <942216465@qq.com>
2019-08-22 15:27:34 +0800

    5 Commit

--- src/table.java
+++ src/table.java
@@ -1,6 +1,7 @@
 public class table {
     private String yqj;
-
+    private   String mzx;
+    private  String wwb;
      public  static  void main (String[] args){
 
          System.out.println("who is your daddy");
