928879f9fb8567dd841bad921667d1d2ccb7ed4c
wwb <18717319321@163.com>
2019-08-22 15:20:46 +0800

    grand father

--- src/table.java
+++ src/table.java
@@ -3,7 +3,7 @@ public class table {
      public  static  void main (String[] args){
 
          System.out.println("who is your daddy");
-
+  System.out.println("I am your grandfather");
 
 
      }
